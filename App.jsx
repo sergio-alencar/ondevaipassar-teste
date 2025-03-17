68f602358bf3f0bed4d146149ba0d651bd80d8c9
Sérgio Alencar <sergiofalencar@gmail.com>
2025-03-17 17:10:27 -0300

    correcoes nos links

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
 
 import React, { useState } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./pages/Home";
